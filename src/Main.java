d8f4329a04fd8c4bb42d90d1a502214ad82c20ff
shvermaNovopay <shubham.verma@novopay.in>
2023-06-22 16:28:57 +0530

    nth node

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-import linkedList.SingleLinkedList;
-import stacks.StacksArray;
-import stacks.StacksLL;
+import algorithms.sorting.SortingAlgo;
 
 public class Main {
     public static void main(String[] args) {
+        SortingAlgo algo = new SortingAlgo();
 
-        StacksLL stack = new StacksLL();
 
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.sortedInsert(3);
-        stack.print();
+        int[] array = {5, 1, 2, 4, 3, 7, 6};
+        algo.insertionSort(array);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ",");
+        }
+
+
     }
 }
--- src/algorithms/sorting/SortingAlgo.java
+++ src/algorithms/sorting/SortingAlgo.java
@@ -2,11 +2,57 @@ package algorithms.sorting;
 
 public class SortingAlgo {
 
+    private static boolean validateEmpty(int[] array) {
+        return array == null || array.length == 0;
+    }
 
-    public void bubbleSort(int[] array){
+    //    WCP= O(n^2)
+//    ACP= O(n^2)
+//    SPACE C= O(1) one temp variable
+    public void bubbleSort(int[] array) {
+        if (validateEmpty(array)) return;
+        int size = array.length, temp;
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
 
+    public void bubbleSortControlledSwap(int[] array) {
+        if (validateEmpty(array)) return;
+        int size = array.length, temp, swaps = 1;
+        for (int i = 0; i < (size - 1) && swaps == 1; i++) {
+            for (int j = 0; j < (size - i - 1); j++) {
+                swaps = 0;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swaps = 1;
+                }
+            }
+        }
+    }
 
+    boolean more(int value1, int value2) {
+        return value1 > value2;
+    }
 
+    public void insertionSort(int[] array) {
+        if (validateEmpty(array)) return;
+        int size = array.length, temp, j;
+        for (int i = 1; i < size; i++) {
+            temp = array[i];
+            for (j = i; j > 0 && more(array[j - 1], temp); j--) {
+                array[j] = array[j - 1];
+            }
+            array[j] = temp;
+        }
     }
 
 
